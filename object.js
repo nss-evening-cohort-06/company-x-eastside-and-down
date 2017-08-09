4d22a675f9c72fb7ced1965ebc7cdb4863285a4d
Ronnie Hickman <rhickman2305@gmail.com>
2017-08-08 20:57:31 -0500

    added object array for product page

---
+++ object.js
@@ -0,0 +1,57 @@
+var allProducts = [];
+
+	var product1 = {
+		name:"Jack Goff",
+		description:"",
+		price:"",
+		image:""
+	};
+
+	var product2 = {
+		name:"Professer X",
+		description:"",
+		price:"",
+		image:""
+	};
+
+	var product3 = {
+		name:"Smart Teacher",
+		description:"",
+		price:"5pies/perClass",
+		image:""
+	};
+
+	var product4 = {
+		name:"Funny Teacher",
+		description:"",
+		price:"5pies/perClass",
+		image:""
+	};
+
+	var product5 = {
+		name:"Old Teacher",
+		description:"",
+		price:"",
+		image:"5pies/perClass"
+	};
+
+	var product6 = {
+		name:"Crap Teacher",
+		description:"",
+		price:"",
+		image:"5pies/perClass"
+	};
+
+	var product7 = {
+		name:"Newbie Teacher",
+		description:"",
+		price:"5pies/perClass",
+		image:""
+	};
+
+	var product8 = {
+		name:"Boring Teacher",
+		description:"",
+		price:"5pies/perClass",
+		image:""
+	};
