13d0523e3b905eedeca24c96709ea5d581978102
Ronnie Hickman <rhickman2305@gmail.com>
2017-08-08 21:03:43 -0500

    updated and added used field

--- object.js
+++ object.js
@@ -4,54 +4,63 @@ var allProducts = [];
 		name:"Jack Goff",
 		description:"",
 		price:"",
-		image:""
+		image:"",
+		used: true false
 	};
 
 	var product2 = {
 		name:"Professer X",
 		description:"",
 		price:"",
-		image:""
+		image:"",
+		used: true false
 	};
 
 	var product3 = {
 		name:"Smart Teacher",
 		description:"",
 		price:"5pies/perClass",
-		image:""
+		image:"";
+		used: true false
 	};
 
 	var product4 = {
 		name:"Funny Teacher",
 		description:"",
 		price:"5pies/perClass",
-		image:""
+		image:"",
+		used: true false
+
 	};
 
 	var product5 = {
 		name:"Old Teacher",
 		description:"",
-		price:"",
-		image:"5pies/perClass"
+		price:"5pies/perClass",
+		image:"",
+		used: true false
 	};
 
 	var product6 = {
 		name:"Crap Teacher",
 		description:"",
-		price:"",
-		image:"5pies/perClass"
+		price:"5pies/perClass",
+		image:"",
+		used: true false
 	};
 
 	var product7 = {
 		name:"Newbie Teacher",
 		description:"",
 		price:"5pies/perClass",
-		image:""
+		image:"",
+		used: true false
 	};
 
 	var product8 = {
 		name:"Boring Teacher",
 		description:"",
 		price:"5pies/perClass",
-		image:""
+		image:"",
+		used: true false
 	};
